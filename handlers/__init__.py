359b9d13392945fefbcceba6b26f29c262d751c1
neuraforgetteam1 <vahid93001h@gmail.com>
2025-06-09 18:31:29 +0330

    Update __init__.py
    # handlers/__init__.py
    from .user import register_user_handlers
    from .admin import register_admin_handlers
    from .marketing import register_marketing_handlers
    from .payment import register_payment_handlers
    
    __all__ = [
        'register_user_handlers',
        'register_admin_handlers',
        'register_marketing_handlers',
        'register_payment_handlers'
    ]

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,1 +1,12 @@
-from .start import register_user_handlers  # اگر در start.py تعریف شده
+# handlers/__init__.py
+from .user import register_user_handlers
+from .admin import register_admin_handlers
+from .marketing import register_marketing_handlers
+from .payment import register_payment_handlers
+
+__all__ = [
+    'register_user_handlers',
+    'register_admin_handlers',
+    'register_marketing_handlers',
+    'register_payment_handlers'
+]
