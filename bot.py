75d34d5f883d2a665472d93a781f21bd93c40da5
neuraforgetteam1 <vahid93001h@gmail.com>
2025-06-09 10:44:03 +0330

    Update bot.py
    from aiogram.fsm.storage.memory import MemoryStorage

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import asyncio
 import logging
 from aiogram import Bot, Dispatcher, types
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.fsm.storage.memory import MemoryStorage
 from config import Config
 from database.db import init_db, close_db
 from handlers import (
